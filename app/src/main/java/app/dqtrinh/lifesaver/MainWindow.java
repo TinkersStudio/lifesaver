7a7eb2ac15023d21dce28502f60a99c3d834e74c
bob <dungtrinh1993@gmail.com>
2016-05-07 16:46:48 -0700

    Add the TODO list

--- app/src/main/java/app/dqtrinh/lifesaver/MainWindow.java
+++ app/src/main/java/app/dqtrinh/lifesaver/MainWindow.java
@@ -7,22 +7,36 @@ import android.os.Bundle;
 import android.support.v7.widget.ButtonBarLayout;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.ImageButton;
 
 public class MainWindow extends AppCompatActivity {
     //update something in here
 
-    protected Button help;
+    protected ImageButton help;
     protected Button setting;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
 
     public void editSetting() {
 
         setting = (Button) findViewById(R.id.settingButton);
+        help = (ImageButton) findViewById(R.id.helpButton);
 
         setting.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent settingWindow = new Intent(MainWindow.this, Setting.class);
                 startActivity(settingWindow);
+            }
+        });
+
+        help.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //TODO: make the call to twilio
+
+                //TODO: make the call to facebook
+                //TODO: Contact autority
             }
         });
     }
@@ -32,11 +46,8 @@ public class MainWindow extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_window);
-
-
         editSetting();
 
     }
-
 
 }
