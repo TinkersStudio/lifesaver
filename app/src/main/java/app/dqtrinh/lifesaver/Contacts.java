4c80895c6eea0aa3095ed251250f111898442229
znbud <zbud@ucdavis.edu>
2016-05-08 09:21:25 -0700

    Layout

---
+++ app/src/main/java/app/dqtrinh/lifesaver/Contacts.java
@@ -0,0 +1,18 @@
+package app.dqtrinh.lifesaver;
+
+/**
+ * Created by Zain on 5/8/2016.
+ */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.view.View;
+public class Contacts extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+
+    }
+}
--- app/src/main/java/app/dqtrinh/lifesaver/Setting.java
+++ app/src/main/java/app/dqtrinh/lifesaver/Setting.java
@@ -1,13 +1,32 @@
 package app.dqtrinh.lifesaver;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
 
-public class Setting extends AppCompatActivity {
+public class Setting extends AppCompatActivity
+{
+    protected Button contact;
+
+    public void editContacts() {
+
+        contact = (Button) findViewById(R.id.contactButton);
+        contact.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent settingContacts = new Intent(getApplicationContext(), Contacts.class);
+                startActivity(settingContacts);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        editContacts();
+
     }
 }
